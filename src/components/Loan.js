ee949ada3da97db3966f30f562fa84ee2c9c7e86
Shivbasav Nandargi <shivagn119@gmail.com>
2019-06-09 20:01:40 +0530

    Second commit

--- src/components/Loan.js
+++ src/components/Loan.js
@@ -5,7 +5,7 @@ export default class Loan extends React.Component {
         return (
             //accessing the props given to the Loan component in App.js
             <div>
-                {this.props.monthlyAmount && <p>Amount to be paid monthly:{this.props.monthlyAmount}</p>}
+                {this.props.monthlyAmount && <p>Amount to be paid monthly in dollars $:{this.props.monthlyAmount}</p>}
                 {this.props.interest && <p>Rate of interest: {this.props.interest}</p>}
                 {this.props.error && <p> {this.props.error}</p>}
             </div>
